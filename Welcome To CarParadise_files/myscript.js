e3c8532ffa7c8415adee839cd381b915ad0f13a8
mishraanmol <anmolmishra54@outlook.com>
2017-11-19 15:55:55 +0530

    Added q1.html for registration page, click on any of the SUV and Buy now button

---
+++ Welcome To CarParadise_files/myscript.js
@@ -0,0 +1,24 @@
+function suv_page()
+{
+	$('#dynamic').load('SUV.html');
+}
+function sedan_page()
+{
+    $('#dynamic').load('sedan.html');	
+}
+function coupes_page()
+{
+    $('#dynamic').load('coupes.html');	
+}
+function offroad_page()
+{
+    $('#dynamic').load('offroad.html');	
+}
+function bikes_page()
+{
+    $('#dynamic').load('bikes.html');	
+}
+function sports_page()
+{
+    $('#dynamic').load('sports.html');	
+}
